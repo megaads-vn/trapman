8c4e469566f9134215595ca568012af8e0ea1712
Bach Nguyen <bachnx2303@gmail.com>
2018-10-16 09:59:44 +0700

    Init package source

---
+++ src/TrapmanServiceProvider.php
@@ -0,0 +1,18 @@
+<?php 
+namespace Megaads\Trapman;
+
+use Illuminate\Support\ServiceProvider;
+
+class TrapmanServiceProvider extends ServiceProvider
+{
+    public function boot()
+    {
+
+    }
+
+    public function register()
+    {
+        
+    }
+
+}
